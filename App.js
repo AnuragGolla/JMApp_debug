1706ccc50ba2820327ae8488c7bedd317712d73c
Anurag Golla <agolla07@gmail.com>
2018-01-26 13:02:57 -0500

    added different text

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+
+
   render() {
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+          <Text style={styles.centerText}>Jock Market App Debugger</Text>
       </View>
     );
   }
@@ -20,4 +21,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  centerText: {
+      color: "blue",
+      fontSize: 30,
+  }
 });
